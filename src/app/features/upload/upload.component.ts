0e2059cf5b0c223b5f98619e2618f703609ce509
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2022-02-22 22:47:04 +0100

    Statistics and upload feature logger and toasts

--- src/app/features/upload/upload.component.ts
+++ src/app/features/upload/upload.component.ts
@@ -3,6 +3,8 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Subject, takeUntil } from 'rxjs';
 import { AccessPoint } from 'src/app/core/models/access-points.model';
 import { AccessPointService } from 'src/app/core/services/access-point.service';
+import { LoggerService } from 'src/app/core/services/logger.service';
+import { ToastService } from 'src/app/core/services/toast.service';
 
 @Component({
   selector: 'app-upload',
@@ -16,7 +18,10 @@ export class UploadComponent implements OnInit, OnDestroy {
   public wigleUploadForm: FormGroup;
   public aircrackngUploadForm: FormGroup;
 
-  constructor(private accessPointService: AccessPointService) { }
+  constructor(
+    private accessPointService: AccessPointService,
+    private toastService: ToastService,
+    private loggerService: LoggerService) { }
   
   ngOnInit(): void {
     this.initializeForms();
@@ -64,10 +69,17 @@ export class UploadComponent implements OnInit, OnDestroy {
    * Temporary workaround. The post endpoint has no json file support.
    */
   public submitUploadDefault(): void {
-    if (!this.defaultUploadForm.valid) return;
+    if (!this.defaultUploadForm.valid) {
+      this.toastService.setError("Select a file to upload it.");
+      return;
+    }
     
     const file = this.defaultUploadForm.get('file').value as File;
-    if (!this.isFileTypeValid(file, 'json')) return;
+    if (!this.isFileTypeValid(file, 'json')) {
+      this.toastService.setError("Invalid file format.");
+      this.defaultUploadForm.reset();
+      return;
+    }
 
     const fileReader = new FileReader();
 
@@ -78,10 +90,12 @@ export class UploadComponent implements OnInit, OnDestroy {
         .pipe(takeUntil(this.destroy$))
         .subscribe({
           complete: () => {
+            this.toastService.setInformation("Upload successful.");
             this.defaultUploadForm.reset();
           },
           error: (error) => {
-            console.error(error);
+            this.loggerService.logError(error);
+            this.toastService.setError("Upload failed.");
           }
         });
     }
@@ -93,19 +107,28 @@ export class UploadComponent implements OnInit, OnDestroy {
    * Submit the upload of the WiGLE scan file
    */
   public submitUploadWigle(): void {
-    if (!this.wigleUploadForm.valid) return;
+    if (!this.wigleUploadForm.valid) {
+      this.toastService.setError("Select a file to upload it.");
+      return;
+    }
     
     const file = this.wigleUploadForm.get('file').value as File;
-    if (!this.isFileTypeValid(file, 'csv')) return;
+    if (!this.isFileTypeValid(file, 'csv')) {
+      this.toastService.setError("Invalid file format.");
+      this.wigleUploadForm.reset();
+      return;
+    }
 
     this.accessPointService.postAccessPointsWigle(file)
       .pipe(takeUntil(this.destroy$))
       .subscribe({
         complete: () => {
+          this.toastService.setInformation("Upload successful.");
           this.wigleUploadForm.reset();
         },
         error: (error) => {
-          console.error(error);
+          this.loggerService.logError(error);
+          this.toastService.setError("Upload failed.");
         }
       });
   }
@@ -114,19 +137,28 @@ export class UploadComponent implements OnInit, OnDestroy {
    * Submit the upload of the Aircrack-ng scan file
    */
   public submitUploadAircrackng(): void {
-    if (!this.aircrackngUploadForm.valid) return;
+    if (!this.aircrackngUploadForm.valid) {
+      this.toastService.setError("Select a file to upload it.");
+      return;
+    }
     
     const file = this.aircrackngUploadForm.get('file').value as File;
-    if (!this.isFileTypeValid(file, 'csv')) return;
+    if (!this.isFileTypeValid(file, 'csv')) {
+      this.toastService.setError("Invalid file format.");
+      this.aircrackngUploadForm.reset();
+      return;
+    }
     
     this.accessPointService.postAccessPointsAircrackng(file)
       .pipe(takeUntil(this.destroy$))
       .subscribe({
         complete: () => {
+          this.toastService.setInformation("Upload successful.");
           this.aircrackngUploadForm.reset();
         },
         error: (error) => {
-          console.error(error);
+          this.loggerService.logError(error);
+          this.toastService.setError("Upload failed.");
         }
       });
   }
