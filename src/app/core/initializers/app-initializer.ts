41f7f3c5b811743ca886c996c9a4c5add75a9a88
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2022-08-25 12:49:55 +0200

    Hotfix: NoServerSpecified case handling

--- src/app/core/initializers/app-initializer.ts
+++ src/app/core/initializers/app-initializer.ts
@@ -2,13 +2,16 @@ import { catchError, firstValueFrom, of } from "rxjs";
 import { AuthService } from "src/app/auth/services/auth.service";
 
 export function appInitializer(authService: AuthService) {
+    if (authService.getServerName() === undefined) return () => Promise<null>;
+    
     const refreshResolveObservable = authService.refreshToken()
         .pipe(catchError((_1, _2) => { 
             authService.clientSideLogout();
             return of(undefined);
-        }))
+        }));
     
-    return (authService.getServerName() !== undefined)
-        ? () => firstValueFrom(refreshResolveObservable)
-        : () => Promise<null>;
+ 
+    return () => (firstValueFrom(refreshResolveObservable).catch((_3) => {
+        return () => Promise<null>;
+    }));
 }
