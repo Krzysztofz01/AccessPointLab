f1436e39981e1488644ceba500f892de841d0f86
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2022-08-25 12:24:03 +0200

    Fix: Param sinkhole APP_INITIALIZER bug fix

--- src/app/core/initializers/app-initializer.ts
+++ src/app/core/initializers/app-initializer.ts
@@ -1,13 +1,11 @@
+import { catchError, firstValueFrom, of } from "rxjs";
 import { AuthService } from "src/app/auth/services/auth.service";
 
 export function appInitializer(authService: AuthService) {
-    try {
-        authService.refreshToken().subscribe({
-            error: () => authService.clientSideLogout()
-        });
-
-        return () => Promise<null>;
-    } catch {
-        return () => Promise<null>;
-    }
+    const refreshResolveObservable = authService.refreshToken()
+        .pipe(catchError((error, result) => { return of(undefined) }))
+    
+    return (authService.getServerName() !== undefined)
+        ? () => firstValueFrom(refreshResolveObservable)
+        : () => Promise<null>;
 }
