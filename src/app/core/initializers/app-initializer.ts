62e0baef0bf6988431e55a23039058128d1cf9db
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2022-08-25 12:28:54 +0200

    Enh: APP_INITIALIZER client side logout on error

--- src/app/core/initializers/app-initializer.ts
+++ src/app/core/initializers/app-initializer.ts
@@ -3,7 +3,10 @@ import { AuthService } from "src/app/auth/services/auth.service";
 
 export function appInitializer(authService: AuthService) {
     const refreshResolveObservable = authService.refreshToken()
-        .pipe(catchError((error, result) => { return of(undefined) }))
+        .pipe(catchError((_1, _2) => { 
+            authService.clientSideLogout();
+            return of(undefined);
+        }))
     
     return (authService.getServerName() !== undefined)
         ? () => firstValueFrom(refreshResolveObservable)
