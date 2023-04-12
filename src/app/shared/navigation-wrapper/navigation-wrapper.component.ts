3b4493e98ffcec443c6e831552bcb0add50c5500
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2023-04-12 14:50:11 +0200

    Enh: Authentication page sidebar support

--- src/app/shared/navigation-wrapper/navigation-wrapper.component.ts
+++ src/app/shared/navigation-wrapper/navigation-wrapper.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/auth/services/auth.service';
 
@@ -7,20 +7,33 @@ import { AuthService } from 'src/app/auth/services/auth.service';
   templateUrl: './navigation-wrapper.component.html',
   styleUrls: ['./navigation-wrapper.component.css']
 })
-export class NavigationWrapperComponent implements OnInit {
+export class NavigationWrapperComponent {
 
   constructor(private authService: AuthService, private router: Router) { }
 
-  ngOnInit(): void {
+  /**
+   * Get a boolean value representing if the user is authenticated
+   * @returns Boolean value representing if the user is authenticated 
+   */
+  public isAuthenticated(): boolean {
+    if (!this.authService.userValue) return false;
+    
+    return true;
   }
 
+  /**
+   * Get the server name
+   * @returns Server name value as string
+   */
   public getServerName(): string {
     return this.authService.getServerName();
   }
 
+  /**
+   * Perform the logout via the authservice and redirect to authentication page
+   */
   public logout(): void {
     this.authService.logout();
     this.router.navigate(['/auth']);
   }
-
 }
